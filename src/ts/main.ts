aebc2787aa38c5348837e0416f733ba0e49ba8e2
Jessica Näsman <jessicanasman@Jessicas-MacBook-Pro.local>
2026-01-15 14:33:26 +0100

    del: hamburger from main

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,45 +2,6 @@ import "../scss/style.scss";
 import { loadProducts, renderProducts } from "./components/ProductCard";
 import type { Product } from "./models/Product";
 
-// hamburger slay
-
-const btnHamburger = document.querySelector<HTMLElement>("#btnHamburger");
-const body = document.body;
-const header = document.querySelector<HTMLElement>(".header");
-const fadeElems = document.querySelectorAll<HTMLElement>(".has-fade");
-
-// safety check
-if (!btnHamburger || !header) {
-  throw new Error("Required DOM elements not found");
-}
-
-btnHamburger.addEventListener("click", (e) => {
-  e.preventDefault(); // prevent page jump
-  console.log("click hamburger");
-
-  const isOpen = header.classList.contains("open");
-
-  if (isOpen) {
-    // close hamburger menu
-    body.classList.remove("noscroll");
-    header.classList.remove("open");
-
-    fadeElems.forEach((element) => {
-      element.classList.remove("fade-in");
-      element.classList.add("fade-out");
-    });
-  } else {
-    // open hamburger menu
-    body.classList.add("noscroll");
-    header.classList.add("open");
-
-    fadeElems.forEach((element) => {
-      element.classList.remove("fade-out");
-      element.classList.add("fade-in");
-    });
-  }
-});
-
 // initialize products
 const init = async () => {
   const products = await loadProducts();
@@ -53,38 +14,42 @@ const init = async () => {
   const newParam = params.get("isNew");
 
   const isNew = newParam === "true";
-  
+
   // get title of page
-  const pageTitle = document.getElementById("product-page-title") as HTMLHeadingElement;
+  const pageTitle = document.getElementById(
+    "product-page-title"
+  ) as HTMLHeadingElement;
 
   let filteredProducts: Product[] = products;
 
   // filter products based on parameter
-  if(categoryParam !== null) {
-    filteredProducts = products.filter((p: Product) => p.category === categoryParam);
+  if (categoryParam !== null) {
+    filteredProducts = products.filter(
+      (p: Product) => p.category === categoryParam
+    );
 
-    if(pageTitle) {
-      pageTitle.textContent = categoryParam.charAt(0).toUpperCase() + categoryParam.slice(1);
+    if (pageTitle) {
+      pageTitle.textContent =
+        categoryParam.charAt(0).toUpperCase() + categoryParam.slice(1);
     }
-    
   } else {
     filteredProducts = products;
   }
 
-  if(isNew) {
+  if (isNew) {
     filteredProducts = products.filter((p: Product) => p.isNew === isNew);
-    if(pageTitle) {
+    if (pageTitle) {
       pageTitle.textContent = "New arrivals";
     }
   }
 
   // sort products so new products are shown first
   const sortedProducts = filteredProducts.sort((a: Product, b: Product) => {
-    if(a.isNew && !b.isNew) {
+    if (a.isNew && !b.isNew) {
       return -1; // a is before b
     }
 
-    if(!a.isNew && b.isNew) {
+    if (!a.isNew && b.isNew) {
       return 1; // a is after b
     }
 
