d4ad1c0bda82d7a4a5d38be7ba7445e1a907fece
amwa00 <wallesamanda@gmail.com>
2026-01-15 19:02:47 +0100

    moved code from main

--- src/ts/components/ProductCard.ts
+++ src/ts/components/ProductCard.ts
@@ -82,3 +82,59 @@ export const renderProducts = (products: Product[]) => {
     grid.append(card);
   });
 };
+
+export const initProductCards = async () => {
+  const products = await loadProducts();
+
+  // create new URLSearchParams objects
+  const params = new URLSearchParams(window.location.search);
+
+  // get correct parameters from url
+  const categoryParam = params.get("category");
+  const newParam = params.get("isNew");
+
+  const isNew = newParam === "true";
+
+  // get title of page
+  const pageTitle = document.getElementById(
+    "product-page-title"
+  ) as HTMLHeadingElement;
+
+  let filteredProducts: Product[] = products;
+
+  // filter products based on parameter
+  if (categoryParam !== null) {
+    filteredProducts = products.filter(
+      (p: Product) => p.category === categoryParam
+    );
+
+    if (pageTitle) {
+      pageTitle.textContent =
+        categoryParam.charAt(0).toUpperCase() + categoryParam.slice(1);
+    }
+  } else {
+    filteredProducts = products;
+  }
+
+  if (isNew) {
+    filteredProducts = products.filter((p: Product) => p.isNew === isNew);
+    if (pageTitle) {
+      pageTitle.textContent = "New arrivals";
+    }
+  }
+
+  // sort products so new products are shown first
+  const sortedProducts = filteredProducts.sort((a: Product, b: Product) => {
+    if (a.isNew && !b.isNew) {
+      return -1; // a is before b
+    }
+
+    if (!a.isNew && b.isNew) {
+      return 1; // a is after b
+    }
+
+    return 0;
+  });
+
+  renderProducts(sortedProducts);
+};
