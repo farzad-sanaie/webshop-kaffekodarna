28b195e9906607831423ae9a59049fd3b42f13d3
Jessica Näsman <jessicanasman@Jessicas-MacBook-Pro.local>
2026-01-18 22:05:37 +0100

    add:form data

---
+++ src/ts/components/ShippingForm.ts
@@ -0,0 +1,14 @@
+const autoFillData: Record<string, string> = {
+  firstName: "Samwise",
+  lastName: "Gamgee",
+  address: "Bagshot Row 3",
+  country: "The Shire",
+  city: "Hobbiton",
+  zip: "123 45",
+  email: "whataboutsecondbreakfast@potatoes.com",
+  phone: "+46 70 123 45 67",
+};
+
+export function initShippingForm() {
+  // all logic lives here
+}
