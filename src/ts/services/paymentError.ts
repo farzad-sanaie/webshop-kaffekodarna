511b45741ad02f33299000198825dc3319f5a7fd
Jessica Näsman <jessicanasman@Jessicas-MacBook-Pro.local>
2026-01-23 12:39:28 +0100

    wrote comments

--- src/ts/services/paymentError.ts
+++ src/ts/services/paymentError.ts
@@ -17,17 +17,20 @@ export function initPaymentError() {
     ".payment__error",
   ) as HTMLParagraphElement | null;
 
+  // click on place order
   confirmButton.addEventListener("click", (event) => {
     event.preventDefault();
 
     const isChecked = Array.from(paymentRadios).some((radio) => radio.checked);
 
     if (!isChecked) {
+      // show error
       paymentSection.classList.add("payment--error");
       if (errorMessage) errorMessage.style.display = "block";
     }
   });
 
+  // remove error when user selects a payment
   paymentRadios.forEach((radio) => {
     radio.addEventListener("change", () => {
       paymentSection.classList.remove("payment--error");
